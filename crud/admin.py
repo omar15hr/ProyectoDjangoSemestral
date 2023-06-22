bfb002d952e5d4ec80cc412f519ca8de1a139502
omar15hr <omarhrn@gmail.com>
2023-06-22 15:53:55 -0400

    Cambios

--- crud/admin.py
+++ crud/admin.py
@@ -2,29 +2,29 @@ from django.contrib import admin
 
 # Register your models here.
 
-class TipoClienteAdmin(admin.ModelAdmin):
-    readonly_fields = ('idTipoCliente','created_at','updated_at')
-    list_display = ('idTipoCliente','nombreTC')
-    ordering = ('idTipoCliente',)
+# class TipoClienteAdmin(admin.ModelAdmin):
+#     readonly_fields = ('idTipoCliente','created_at','updated_at')
+#     list_display = ('idTipoCliente','nombreTC')
+#     ordering = ('idTipoCliente',)
 
-class ClienteAdmin(admin.ModelAdmin):
-    readonly_fields = ('created_at','updated_at')
-    list_display = ('rutCliente','dvRutCliente','nombreCliente','telefonoCliente','emailCliente','idTipoCliente')
-    ordering = ('rutCliente',)
+# class ClienteAdmin(admin.ModelAdmin):
+#     readonly_fields = ('created_at','updated_at')
+#     list_display = ('rutCliente','dvRutCliente','nombreCliente','telefonoCliente','emailCliente','idTipoCliente')
+#     ordering = ('rutCliente',)
 
-class TipoProductoAdmin(admin.ModelAdmin):
-    readonly_fields = ('idTipoProducto','created_at','updated_at')
-    list_display = ('idTipoProducto','nombreTP')
-    ordering = ('idTipoProducto',)
+# class TipoProductoAdmin(admin.ModelAdmin):
+#     readonly_fields = ('idTipoProducto','created_at','updated_at')
+#     list_display = ('idTipoProducto','nombreTP')
+#     ordering = ('idTipoProducto',)
 
-class ProductoAdmin(admin.ModelAdmin):
-    readonly_fields = ('idProducto','created_at','updated_at')
-    list_display = ('idProducto','nombreProducto','precioProducto','stockProducto','idTipoProducto')
-    ordering = ('idProducto','idTipoProducto',)
+# class ProductoAdmin(admin.ModelAdmin):
+#     readonly_fields = ('idProducto','created_at','updated_at')
+#     list_display = ('idProducto','nombreProducto','precioProducto','stockProducto','idTipoProducto')
+#     ordering = ('idProducto','idTipoProducto',)
 
 
-admin.site.register(TipoCliente,TipoClienteAdmin)
-admin.site.register(Cliente,ClienteAdmin)
-admin.site.register(TipoProducto,TipoProductoAdmin)
-admin.site.register(Producto,ProductoAdmin)
+# admin.site.register(TipoCliente,TipoClienteAdmin)
+# admin.site.register(Cliente,ClienteAdmin)
+# admin.site.register(TipoProducto,TipoProductoAdmin)
+# admin.site.register(Producto,ProductoAdmin)
 
