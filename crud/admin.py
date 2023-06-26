02305822c7e1460fecb2ff38f456bc6c5c977df6
javierh0815 <131109803+javierh0815@users.noreply.github.com>
2023-06-26 12:23:10 -0400

    admin comentado

--- crud/admin.py
+++ crud/admin.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
-from models import TipoCliente,Cliente,TipoProducto,Producto
+# from django.contrib import admin
+# from models import TipoCliente,Cliente,TipoProducto,Producto
 
 # Register your models here.
 
